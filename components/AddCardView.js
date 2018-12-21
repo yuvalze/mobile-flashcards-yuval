0e907e212d1bb137060319b8cd8fcc42e650e306
Yuval Zehavi <none>
2018-12-21 21:11:57 +0200

    Add New Card - bare-bones component

--- components/AddCardView.js
+++ components/AddCardView.js
@@ -1,12 +1,71 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TextInput, StyleSheet, TouchableOpacity, Platform} from 'react-native'
+import { purple, white } from '../utils/colors'
+
+function SubmitBtn ({ onPress }) {
+    return (
+      <TouchableOpacity
+        style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+        onPress={onPress}>
+          <Text style={styles.submitBtnText}>SUBMIT</Text>
+      </TouchableOpacity>
+    )
+  }
 
 export default class AddCardView extends React.Component {
+    state = {
+        questionText: '',
+        answerText: ''
+    }
+
+    onSubmit = () => {
+
+    }
+    
     render() {
         return (
             <View>
-                <Text> AddCard </Text>
-            </View>
+                <Text> The New Question</Text>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(questionText) => this.setState({questionText})}
+                    value={this.state.questionText}
+                />
+                <Text> The New Answer</Text>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(answerText) => this.setState({answerText})}
+                    value={this.state.answerText}
+                />
+                <SubmitBtn onPress={this.onSubmit} />
+          </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    iosSubmitBtn: {
+      backgroundColor: purple,
+      padding: 10,
+      borderRadius: 7,
+      height: 45,
+      marginLeft: 40,
+      marginRight: 40,
+    },
+    AndroidSubmitBtn: {
+      backgroundColor: purple,
+      padding: 10,
+      paddingLeft: 30,
+      paddingRight: 30,
+      height: 45,
+      borderRadius: 2,
+      alignSelf: 'flex-end',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    submitBtnText: {
+      color: white,
+      fontSize: 22,
+      textAlign: 'center',
+    },
+  })
