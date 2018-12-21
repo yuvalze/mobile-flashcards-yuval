661849294e6d0d81179319899f035db9a56f79fc
Yuval Zehavi <none>
2018-12-22 00:31:09 +0200

    Start the Quiz

--- components/CardView.js
+++ components/CardView.js
@@ -1,12 +1,67 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TextInput, StyleSheet, TouchableOpacity, Platform} from 'react-native'
+import { purple, white } from '../utils/colors'
+
+
+function ChangeSideBtn ({ onPress }) {
+    return (
+      <TouchableOpacity
+        style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+        onPress={onPress}>
+          <Text style={styles.changeSideBtnText}>Change Side</Text>
+      </TouchableOpacity>
+    )
+  }
 
 export default class CardView extends React.Component {
+    state = {
+        cardSide: 'question'
+    };
+    OnChangeSize = () => {
+        if (this.state.cardSide === 'question') {
+            this.setState({cardSide: 'answer'});
+        }
+        else {
+            this.setState({cardSide: 'question'});
+        }
+    }
     render() {
+        console.log('CardView props');
+        console.log(this.props);
+        const titleText = this.state.cardSide === 'question' ?  'Question' : 'Answer';
         return (
             <View>
-                <Text> CardView </Text>
+                <Text> {titleText} </Text>
+                <Text> {this.props.cardData[this.state.cardSide]} </Text>
+                <ChangeSideBtn onPress={this.OnChangeSize} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    iosSubmitBtn: {
+      backgroundColor: purple,
+      padding: 10,
+      borderRadius: 7,
+      height: 45,
+      marginLeft: 40,
+      marginRight: 40,
+    },
+    AndroidSubmitBtn: {
+      backgroundColor: purple,
+      padding: 10,
+      paddingLeft: 30,
+      paddingRight: 30,
+      height: 45,
+      borderRadius: 2,
+      alignSelf: 'flex-end',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    changeSideBtnText: {
+      color: white,
+      fontSize: 22,
+      textAlign: 'center',
+    },
+  })
--- components/QuizView.js
+++ components/QuizView.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import CardView from './CardView'
 
 export default class QuizView extends React.Component {
     render() {
+        const {deckItem}  = this.props.navigation.state.params;
         return (
             <View>
                 <Text> QuizView </Text>
+                <CardView cardData={deckItem.questions[0]}/>
             </View>
         )
     }
